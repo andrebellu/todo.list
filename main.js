064a985abc6335b9423c181405b223adccc60e88
andrebellu <bellu.andrea@outlook.it>
2022-10-20 08:06:18 +0200

    .

--- main.js
+++ main.js
@@ -1,55 +1,62 @@
-import "./style.css";
+import "./style.css"
 
 class Nota {
   constructor(titolo) {
-    this.titolo = titolo;
+    this.titolo = titolo
   }
 }
 
-const tasks = [];
+let tasks = []
 
-const newTask = document.getElementById("newTask");
-const newBtn = document.getElementById("newBtn");
-const taskList = document.getElementById("taskList");
+if(localStorage.getItem("notes")) {
+  tasks = JSON.parse(localStorage.getItem("notes"))
+}
+
+const newTask = document.getElementById("newTask")
+const newBtn = document.getElementById("newBtn")
+const taskList = document.getElementById("taskList")
 
 const updateTasks = () => {
-  taskList.innerHTML = "";
+  taskList.innerHTML = ""
 
   for (const task of tasks) {
-    const taskDiv = document.createElement("div");
-    taskDiv.className = "custom-list-item";
+    const taskDiv = document.createElement("div")
+    taskDiv.className = "custom-list-item"
 
-    const el = document.createElement("div");
-    el.className = "custom-task";
-    el.innerText = task.titolo;
+    const el = document.createElement("div")
+    el.className = "custom-task"
+    el.innerText = task.titolo
 
     el.onclick = () => {
-      el.classList.toggle("custom-task-done");
-    };
+      el.classList.toggle("custom-task-done")
+    }
 
-    const bin = document.createElement("div");
-    bin.innerText = "x";
-    bin.className = "custom-btn";
+    const bin = document.createElement("div")
+    bin.innerText = "x"
+    bin.className = "custom-btn"
     bin.onclick = () => {
-      const index = tasks.indexOf(task);
-      tasks.splice(index, 1);
-      updateTasks();
-    };
+      const index = tasks.indexOf(task)
+      tasks.splice(index, 1)
+      updateTasks()
+    }
 
-    taskDiv.appendChild(el);
-    taskDiv.appendChild(bin);
+    taskDiv.appendChild(el)
+    taskDiv.appendChild(bin)
 
-    taskList.appendChild(taskDiv);
+    taskList.appendChild(taskDiv)
   }
-};
+}
+
+updateTasks()
 
 newBtn.onclick = () => {
-  const task = new Nota(newTask.value);
-  newTask.value = "";
-  tasks.push(task);
-  updateTasks();
+  const task = new Nota(newTask.value)
+  newTask.value = ""
+  tasks.push(task)
+  localStorage.setItem("notes", JSON.stringify(tasks))
+  updateTasks()
 
-  console.log(tasks);
-};
+  console.log(tasks)
+}
 
-updateTasks();
+updateTasks()
